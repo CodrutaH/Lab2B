b0b932c3b3401f1f60b8e1f933707ce86898fc6f
CodrutaH <50257985+CodrutaH@users.noreply.github.com>
2019-05-21 19:28:57 +0300

    INITIAL COMMIT

---
+++ Lab2B/Controllers/MoviesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2B.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab2B.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private Models.MoviesDbContext context;
+        public MoviesController(Models.MoviesDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Flowers
+        [HttpGet]
+        public IEnumerable<Models.Movie> Get([FromQuery]DateTime? from, [FromQuery]DateTime? to)
+        {
+            IQueryable<Movie> result = context.Movies;
+            if (from == null && to == null)
+            {
+                return result;
+            }
+            if (from != null)
+            {
+                result = result.Where(m => m.Date >= from);
+
+            }
+            if (to != null)
+            {
+                result = result.Where(m => m.Date <= to);
+            }
+
+          
+
+            return result;
+
+        }
+
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var existing = context.Movies.FirstOrDefault(movie => movie.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(existing);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            //if (!ModelState.IsValid)
+            //{
+
+            //}
+            context.Movies.Add(movie);
+            context.SaveChanges();
+        }
+
+        // PUT: api/Flowers/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Movie movie)
+        {
+            var existing = context.Movies.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (existing == null)
+            {
+                context.Movies.Add(movie);
+                context.SaveChanges();
+                return Ok(movie);
+            }
+            movie.Id = id;
+            context.Movies.Update(movie);
+            context.SaveChanges();
+            return Ok(movie);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = context.Movies.FirstOrDefault(movie => movie.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+            context.Movies.Remove(existing);
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
